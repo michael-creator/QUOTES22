4fdb30764962f7685e8b8a9c586d103b5cdcb3d5
michael <mikelkarije@gmail.com>
2019-10-27 03:17:11 +0300

    addition of forms to my work to take in user input

---
+++ src/app/drunkquote-form/drunkquote-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-drunkquote-form',
+  templateUrl: './drunkquote-form.component.html',
+  styleUrls: ['./drunkquote-form.component.css']
+})
+export class DrunkquoteFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
