71bf5e7ccd325ae2bea2fea2ed8ad6cfdd2f383f
michael <mikelkarije@gmail.com>
2019-10-28 16:37:39 +0300

    declared properties upvotes and downvotes and also including styling

--- src/app/drunkquote-detail/drunkquote-detail.component.ts
+++ src/app/drunkquote-detail/drunkquote-detail.component.ts
@@ -8,9 +8,17 @@ import { Drunkquote } from '../drunkquote';
 export class DrunkquoteDetailComponent implements OnInit {
 @Input() drunkquote: Drunkquote
 @Output() isComplete = new EventEmitter<boolean>();
+@Output() upvotes = new EventEmitter<boolean>();
+@Output() downvotes = new EventEmitter<boolean>();
 
 drunkquoteDelete(complete:boolean){
   this.isComplete.emit(complete);
+}
+upVotes(){
+  this.upvotes.emit(true);
+}
+downVotes(){
+  this.downvotes.emit(true);
 }
   constructor() { }
 
--- src/app/drunkquote/drunkquote.component.ts
+++ src/app/drunkquote/drunkquote.component.ts
@@ -16,10 +16,12 @@ export class DrunkquoteComponent implements OnInit {
     new Drunkquote(4,'Benjamin Franklin','In wine there is wisdom, in beer there is Freedom, in water there is bacteria.',new Date(2019,10,9)),
     new Drunkquote(5,'Winston S. Churchill','A lady came up to me one day and said ‘Sir! You are drunk’, to which I replied ‘I am drunk today madam, and tomorrow I shall be sober, but you will still be ugly.',new Date(2019,10,9)),
   ];
+  upvotes: any;
+  downvotes: any;
 
-  ngAfterViewInit(){
-    this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = '#1bbfa7';
- }
+//   ngAfterViewInit(){
+//     this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = '#1bbfa7';
+//  }
 
   addNewDrunkquote(drunkquote){
     let drunkquoteLength = this.drunkquotes.length;
@@ -31,17 +33,23 @@ export class DrunkquoteComponent implements OnInit {
   toggleDetails(index){
     this.drunkquotes[index].showDescription = !this.drunkquotes[index].showDescription;
   }
- 
   deleteDrunkquote(isComplete, index){
     if (isComplete) {
       let toDelete = confirm(`Are you sure you want to delete ${this.drunkquotes[index].name}?`)
 
       if (toDelete){
-        this.drunkquotes.splice(index,1)
+        this.drunkquotes.splice(index,1) // m = m+1
       }
     }
   }
+  upVotes(upvotes,index){
+      this.drunkquotes[index].upvotes += 1;
+  };
+  downVotes(downvotes,index){
+    this.drunkquotes[index].downvotes += 1;
+};
   
+
   constructor(private elementRef: ElementRef) { }
  
   ngOnInit() {
